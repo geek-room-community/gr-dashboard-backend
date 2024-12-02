c1b46a687be2356c7efa681ef836a76ff14c8cd7
Shiven Goomer <shivengoomer@gmail.com>
2024-12-02 15:15:45 +0530

    .env.dist created Readme Updated

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ api=Api(app)
 load_dotenv()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv("Track-Modifications")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv("TRACK_MODIFICATION")
 
 db.init_app(app)
 #flask_restful routes
